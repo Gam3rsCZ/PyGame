bc2b089a40924be63e8bbd205f6255c298dc7793
David Stanovský <warthunderaccout@seznam.cz>
2024-01-23 07:18:44 +0100

    3rd push (Added image) (Added Projectiles)

--- First try/game.py
+++ First try/game.py
@@ -6,19 +6,25 @@ class Game():
 
         # pygame setup
         pygame.init()
-        self.screen = pygame.display.set_mode((1920, 1080))
+        self.screenSize = {"WIDTH": pygame.display.Info().current_w, "HEIGHT": pygame.display.Info().current_h}
+        self.screen = pygame.display.set_mode((800,600))#((self.screenSize["WIDTH"], self.screenSize["HEIGHT"]))
+        #pygame.display.toggle_fullscreen()
         self.clock = pygame.time.Clock()
         self.running = True
         self.FPS_LIMIT = 60
+        self.icon = pygame.image.load("woman.png").convert()
+        pygame.display.set_caption("My first game")
+        pygame.display.set_icon(self.icon)
     
         # declaring variables
         self.radius = 7.5
         self.speed = 7.5
-        self.dots = []
-        self.oldest = 0
-        self.maxDots = 5
+        self.projectiles = []
+        self.maxProjectiles = 5
+        self.projectileSpeed = (self.speed/3)*2
         self.playerPosition = pygame.Vector2(self.screen.get_width()/2, self.screen.get_height()/2)
         self.border = {"LEFT": 0 + self.radius, "RIGHT": self.screen.get_width() - self.radius, "TOP": 0 + self.radius, "BOTTOM": self.screen.get_height() - self.radius}
+        self.resizedIcon = pygame.transform.scale(self.icon, (self.screen.get_width(), self.screen.get_height()))
         
     def gameLoop(self):
         while self.running:
@@ -26,11 +32,15 @@ class Game():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.screen.fill("black")
+            self.screen.blit(self.resizedIcon, (0,0),)
 
-            for dot in self.dots:
-                pygame.draw.circle(surface=self.screen, color="red", center=(dot["X"], dot["Y"]), radius=self.radius/2)
-            
+            for projectile in self.projectiles:
+                if (projectile["X"] > self.screen.get_width()) or (projectile["Y"] > self.screen.get_height()) or (projectile["X"] < 0) or (projectile["Y"] < 0):
+                    self.projectiles.remove({"X": projectile["X"],"Y": projectile["Y"]})
+                else:
+                    projectile["X"] += 5
+                    pygame.draw.circle(surface=self.screen, color="red", center=(projectile["X"], projectile["Y"]), radius=self.radius/2)
+                
             pygame.draw.circle(surface=self.screen, color="white", center=(self.playerPosition), radius=self.radius)
             
             self.controls()
@@ -64,17 +74,13 @@ class Game():
             self.playerPosition.x += self.speed
         if keys[pygame.K_ESCAPE]:
             self.running = False
-        if keys[pygame.K_w]:
-            if {"X": self.playerPosition.x, "Y": self.playerPosition.y} in self.dots:
+        if keys[pygame.K_SPACE]:
+            if {"X":self.playerPosition.x, "Y":self.playerPosition.y} in self.projectiles:
                 return
-            elif len(self.dots) < self.maxDots:
-                self.dots.append({"X": self.playerPosition.x, "Y": self.playerPosition.y})
             else:
-                if self.oldest < self.maxDots:
-                    self.dots.pop(self.oldest)
-                    self.dots.insert(self.oldest, {"X": self.playerPosition.x, "Y": self.playerPosition.y})
-                    self.oldest +=1
+                if len(self.projectiles) < self.maxProjectiles:
+                    self.projectiles.append({"X":self.playerPosition.x, "Y":self.playerPosition.y})
                 else:
-                    self.oldest = 0
+                    return    
         if keys[pygame.K_s]:
-            self.dots.clear()
+            self.projectiles.clear()
